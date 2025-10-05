22260085cba7ec15598aa8615ca1d178edd67fe0
Daisuke AKAGAWA <daisuke1024akagawa@gmail.com>
2025-10-05 20:58:13 +0900

    :sparkles: Create variants set sample

---
+++ scripts/create_variant_sets.py
@@ -0,0 +1,33 @@
+from pxr import Usd, UsdGeom, Gf
+
+stage = Usd.Stage.CreateNew("./assets/create_variant_prims.usda")
+world = UsdGeom.Xform.Define(stage, "/World")
+stage.SetDefaultPrim(world.GetPrim())
+
+box = UsdGeom.Xform.Define(
+    stage,
+    world.GetPath().AppendPath("Box")
+)
+geo_scope = UsdGeom.Scope.Define(
+    stage,
+    box.GetPath().AppendPath("Geometry")
+)
+
+geo_variant_sets = geo_scope.GetPrim().GetVariantSets()
+shapes_variant_set = geo_variant_sets.AddVariantSet("shapes")
+shapes_variant_set.AddVariant("Cube")
+shapes_variant_set.AddVariant("Sphere")
+
+shapes_variant_set.SetVariantSelection("Cube")
+
+with shapes_variant_set.GetVariantEditContext():
+    cube = UsdGeom.Cube.Define(
+        stage,
+        geo_scope.GetPath().AppendPath("Cube")
+    )
+    cube_prim = cube.GetPrim()
+    UsdGeom.XformCommonAPI(cube_prim).SetTranslate(
+        Gf.Vec3d(1, 0, 0)
+    )
+    cube_prim.SetTypeName("Cube")
+stage.Save()
